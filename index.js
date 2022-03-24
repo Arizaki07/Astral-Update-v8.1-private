dcbfa13e46e04c29f82b1e838d3e44f7aaf789aa
Arizaki07 <102314211+Arizaki07@users.noreply.github.com>
2022-03-24 21:32:08 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const Astral = require('astral.js');
+code.execute = Discord;
+
+const bot = [
+  Token: "Token du bot",
+  Prefix: "Prefix du bot",
+  Key: "Key d'utilisation"
+ ]
+
+bot.active.key ///Test la key d'activation
+
+Command.handler.on = require('./commands');
+
+[
+  Nom: "ping",
+  code: `$ping ms.`
+ ]
+
